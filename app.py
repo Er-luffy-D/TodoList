8e8da11242887ad2fa8a287071d25ecdc72e3b71
Piyush <02jan06@gmail.com>
2024-04-28 03:10:24 +0530

    added requirements.txt

--- app.py
+++ app.py
@@ -8,11 +8,12 @@ app.config['SQLALCHEMY_DATABASE_URI']= "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
 db=SQLAlchemy(app)
 
+# database structure
 class Todo(db.Model):
     sno=db.Column(db.Integer,primary_key=True)
     title=db.Column(db.String(200),nullable=False)
     desc=db.Column(db.String(500),nullable=False) 
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
+    date_created=db.Column(db.DateTime,default=datetime.now())
 
     def __repr__(self) -> str:
         return f"{self.sno}-{self.title}"
@@ -33,15 +34,21 @@ def add():
 
     return render_template("index.html",allTodo=allTodo)
 
-@app.route("/login")
-def hllo():
-    return "hello world"
 
-@app.route("/Update/<int:sno>")
+@app.route("/Update/<int:sno>",methods=["GET","POST"])
 def update(sno):
-    # todo=Todo.query.filter_by(sno=sno).first()
-    # db.session.
-    return "hello world"
+    if request.method=="POST":
+        title=request.form["title"]
+        desc=request.form["desc"]
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html", todo=todo)
+
 
 @app.route("/Delete/<int:sno>")
 def delete(sno):
